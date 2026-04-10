2cbef61f91687c76ac0e07047f96354384454d7a
Archie <archiealvarado20@gmail.com>
2026-04-10 23:47:38 +0800

    fixed prisma

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,9 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import prisma from "./prisma";
+import { PrismaClient } from "./generated/prisma";
 import { nextCookies } from "better-auth/next-js";
 
+const prisma = new PrismaClient();
 export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: "postgresql" }),
   emailAndPassword: {
