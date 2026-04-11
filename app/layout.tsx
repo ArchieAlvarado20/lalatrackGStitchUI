73cd72c7a3ffa7e56e63ffb0fb2a7f7a89e3e9ee
Archie <archiealvarado20@gmail.com>
2026-04-11 19:30:15 +0800

    Analytics Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,7 +36,7 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-screen bg-[#0e0e0e] text-white font-['Inter'] selection:bg-[#f26722] pb-32">
-        {children} <Toaster />
+        {children} <Toaster /> <Analytics />
       </body>
     </html>
   );
