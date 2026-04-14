842ec4375eb35c7b861bc086b60f7e8ca2836c2e
Archie <archiealvarado20@gmail.com>
2026-04-14 20:06:22 +0800

    buttomNav

--- components/loading.tsx
+++ components/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       {/* Spinner container */}
-      <div className="relative flex items-center justify-center">
+      <div className="relative flex items-center justify-center my-auto">
         {/* Spinner */}
         <div className="animate-spin rounded-full h-14 w-14 border-4 border-orange-500 border-t-transparent" />
 
